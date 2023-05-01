dad5b191818a5c8243775428d04b23ba18ee1590
hindawi <ahmedhindawi2001@gmail.com>
2023-05-01 22:41:57 +0300

    before tensorflow

--- model/test.py
+++ model/test.py
@@ -16,10 +16,49 @@ def random_board(max_depth=200):
     return board
 
 def stockfish(board, depth):
-  with chess.engine.SimpleEngine.popen_uci('./stockfish') as sf:
+  with chess.engine.SimpleEngine.popen_uci('./content/stockfish') as sf:
     result = sf.analyse(board, chess.engine.Limit(depth=depth))
     score = result['score'].white().score()
     return score
 
 board = random_board()
 board
+
+squares_index = {
+    'a':0,
+    'b':1,
+    'c':2,
+    'd':3,
+    'e':4,
+    'f':5,
+    'g':6,
+    'h':7
+}
+
+# example h3 -> 17
+def square_to_index(square):
+   letter = chess.square_name(square)
+   return 8 - int(letter[1]), squares_index[letter[0]]
+
+def split_dims(board):
+    board3d = numpy.zeros((14, 8, 8), dtype=numpy.int8)
+    for piece in chess.PIECE_TYPES:
+        for square in board.pieces(piece, chess.WHITE):
+            idx = numpy.unravel_index(square, (8, 8))
+            board3d[piece - 1][7 - idx[0]][idx[1]] = 1
+        for square in board.pieces(piece, chess.BLACK):
+            idx = numpy.unravel_index(square, (8, 8))
+            board3d[piece + 5][7 - idx[0]][idx[1]] = 1
+
+    aux = board.turn
+    board.turn = chess.WHITE
+    for move in board.legal_moves:
+        i, j = square_to_index(move.to_square)
+        board3d[12][i][j] = 1
+    board.turn = chess.BLACK
+    for move in board.legal_moves:
+        i, j = square_to_index(move.to_square)
+        board3d[13][i][j] = 1
+    board.turn = aux
+
+    return board3d
