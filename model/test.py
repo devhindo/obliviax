87c7b6c0f15084e8c1f082a05df6d2365cb4a7b3
hindawi <ahmedhindawi2001@gmail.com>
2023-05-01 20:45:41 +0300

    grrrr

--- model/test.py
+++ model/test.py
@@ -9,4 +9,17 @@ def random_board(max_depth=200):
 
     for _ in range(depth):
         all_moves = list(board.legal_moves)
-        
+        random_move = random.choice(all_moves)
+        board.push(random_move)
+        if board.is_game_over():
+            break
+    return board
+
+def stockfish(board, depth):
+  with chess.engine.SimpleEngine.popen_uci('./stockfish') as sf:
+    result = sf.analyse(board, chess.engine.Limit(depth=depth))
+    score = result['score'].white().score()
+    return score
+
+board = random_board()
+board
